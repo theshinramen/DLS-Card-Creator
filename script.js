3a7003d3c04a5ab41bd8154c2cd553b8c06aaf26
theshinramen <109312506+theshinramen@users.noreply.github.com>
2024-04-24 19:45:04 -0700

    Update links

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ setInterval(function () {
     document.body.style.backgroundImage = `url(assets/bg/${versions[i]}.png)`;
     document.getElementById('card').src = `assets/card/${versions[i]}.png`;
     document.getElementById('logo').src = `assets/logo/${versions[i]}.png`;
-    document.getElementById('link').href = `/DLS-Card-Creator/DLS${versions[i]}`;
+    document.getElementById('link').href = `/DLS${versions[i]}`;
     i += 1;
     if (i == versions.length) {
         i = 0;
